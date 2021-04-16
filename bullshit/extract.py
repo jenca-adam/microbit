2f6e732a6ad49fdeee91ccc0857c718dc738445e
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 11:23:08 +0200

    new data!

--- bullshit/extract.py
+++ bullshit/extract.py
@@ -16,8 +16,13 @@ class Example(Frame):
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
+        data=[]
         with open('data.txt') as f :
-            for line in f
+            for line in f:
+                temp,time=tuple(line.split(';'))
+                data.append(temp)
+                data.append(round(time-600000))
+        canvas.create_line(*data)
         canvas.pack(fill=BOTH, expand=1)
 
 
