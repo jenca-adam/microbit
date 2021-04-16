397ebcde6d7688604fd19a7e6a3cc5b2673fdb35
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 11:42:20 +0200

    new data!

--- bullshit/extract.py
+++ bullshit/extract.py
@@ -23,7 +23,7 @@ class Example(Frame):
                 temp,time=tuple(line.split(';'))
                 data.append(c)
 
-                data.append(1005-int(temp)*2)
+                data.append(1005-int(temp)*20)
                 c+=10
         print(*data)
         if len(data)>190:
