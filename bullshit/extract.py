336aece3b59fb0335c21926e740451c11723dab8
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 12:27:30 +0200

    new data!

--- bullshit/extract.py
+++ bullshit/extract.py
@@ -9,15 +9,9 @@ class Example(Frame):
         self.root=root
         self.initUI()
     def _reset(self,foo=None):
-        del self.canvas
-        self.__init__(self.root)
-    def initUI(self,foo=None,h=None):
-        if h is None:
-            h=self.winfo_screenheight()
-        self.master.title("Lines")
-        self.pack(fill=BOTH, expand=1)
-        self.canvas = Canvas(self)
-
+        self.canvas.delete('all')
+        self.draw(foo.height)
+    def draw(self,h):
         data=[]
         c=0
         with open('data.txt') as f :
@@ -34,6 +28,15 @@ class Example(Frame):
         self.canvas.create_line(*data,width=3)
         self.canvas.pack(fill=BOTH, expand=1)
         self.canvas.bind('<Configure>',self._reset)
+
+
+    def initUI(self,foo=None,h=None):
+        if h is None:
+            h=self.winfo_screenheight()
+        self.master.title("Lines")
+        self.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self)
+        draw(h)
 
 def main(foo=None):
 
