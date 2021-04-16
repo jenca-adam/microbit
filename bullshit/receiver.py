f04a3077c7af990c66345afc05f94dc349395b56
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 11:29:49 +0200

    new data!

--- bullshit/receiver.py
+++ bullshit/receiver.py
@@ -3,6 +3,7 @@ import os
 import time
 cache=""
 for i in Microbit():
-    if i!=cache and i:
+    if (i!=cache or i!='\n') and i:
+
         print(f'{i[:-1]};{time.time()}\n',file=open("data.txt",'a'))
         cache=i
