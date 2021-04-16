2edba96ebf559ccd2b87bfa5d44b824c460486ec
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 12:12:38 +0200

    new data!

--- bullshit/extract.py
+++ bullshit/extract.py
@@ -11,6 +11,7 @@ class Example(Frame):
         self.h=h
         self.initUI()
     def _reset(self,foo=None):
+        self.destroy()
         self=Example(self.root,foo.width,foo.height)
     def initUI(self,foo=None):
         print(foo)
