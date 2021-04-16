38aa061115382fcca77280a3c427952e7248a5f8
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 11:55:18 +0200

    new data!

--- bullshit/extract.py
+++ bullshit/extract.py
@@ -23,7 +23,7 @@ class Example(Frame):
                 temp,time=tuple(line.split(';'))
                 data.append(c)
 
-                data.append((1005-int(temp)*5)+500)
+                data.append((1005-int(temp)*5)+50)
                 c+=5
         
         
