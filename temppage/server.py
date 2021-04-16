7edbad8bb11163f624aaf15f6fd031b8b61f8f91
Adam Jenča <jenca.adam@gmail.com>
2021-04-16 12:49:11 +0200

    new data!

---
+++ temppage/server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import *
+app=Flask(__name__)
+@app.route('/'):
+def index():
+        data=[]
+        c=0
+        with open('data.txt') as f :
+            for line in f:
+                if line=='\n':
+                    continue
+                temp,time=tuple(line.split(';'))
+                if not temp:
+                    continue
+                data.append(c)
+
+                data.append((h-int(temp)*5)-h//2)
+                c+=1
+        return render_template('index.html',data=data)
+app.run()
+    
