25aa1a6f77915a9036810017e3d77e21a3180586
Adam Jenča <jenca.adam@gmail.com>
2021-04-21 12:15:45 +0200

    aa

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
-APPSRC="https://raw.githubusercontent.com/jenca-adam/microbit/raw/master/recaptcha"
-cd
+APPSRC="https://raw.githubusercontent.com/jenca-adam/microbit/master/recaptcha"
+DSKSRC="https://raw.githubusercontent.com/jenca-adam/microbit/master/rec.desktop"
+IMGSRC="https://pbs.twimg.com/profile_images/566388455265931264/dVHQiE0t.png"
+cd ~
 echo "Downloading sources"
 curl $APPSRC>recaptcha
 echo "installing python"
@@ -8,4 +10,11 @@ yay -S python
 echo "Installing Python libraries"
 pip install whitenoise_player
 pip install art
+echo "syncing db"
+sleep 1
+echo "Creating desktop file"
+curl $DSKSRC >rc.desktop
+cd ~
+echo "Downloading icon"
+curl $IMGSRC >rc.png
 
